8b1da64398ac428901e1f8be8332a93ec28b64a2
m-karakus <s.metinkarakus@gmail.com>
2024-12-12 14:19:17 +0300

    mariadb jars added

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='mkpipe-loader-mariadb',
     version='0.1.0',
     license='Apache License 2.0',
-    packages=find_packages(exclude=['tests', 'scripts', 'deploy']),
+    packages=find_packages(exclude=['tests', 'scripts', 'deploy', 'install_jars.py']),
     install_requires=['mkpipe'],
     include_package_data=True,
     entry_points={
