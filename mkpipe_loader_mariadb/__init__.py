40ae5a09538985be47f64df85113d600804786b4
m-karakus <s.metinkarakus@gmail.com>
2024-12-14 15:24:39 +0300

    feat: unnecessary variable cleanup

--- mkpipe_loader_mariadb/__init__.py
+++ mkpipe_loader_mariadb/__init__.py
@@ -25,11 +25,7 @@ class MariadbLoader:
 
         self.driver_name = 'mysql'
         self.driver_jdbc = 'org.mariadb.jdbc.Driver'
-
-        script_dir = Path(__file__).parent  # Directory where the script is located
-        self.settings.jars_path = str(script_dir / 'jars') + '/*'
-        self.settings.driver_name = 'mariadb'
-
+        self.settings.driver_name = self.driver_name 
         self.jdbc_url = f'jdbc:{self.driver_name}://{self.host}:{self.port}/{self.database}?user={self.username}&password={self.password}'
 
         config = load_config()
